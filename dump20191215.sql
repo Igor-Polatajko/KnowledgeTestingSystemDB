37b7cb79659e92c78a9269c4aa49c7da81a310e8
Igor-Polatajko <igor.bogdanovich39@gmail.com>
2019-12-15 19:23:34 +0200

    Added dump

---
+++ dump20191215.sql
@@ -0,0 +1,392 @@
+-- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+--
+-- Host: localhost    Database: knowledgetestingdb
+-- ------------------------------------------------------
+-- Server version	8.0.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `answers`
+--
+
+DROP TABLE IF EXISTS `answers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `answers` (
+  `answer_id` int(32) NOT NULL AUTO_INCREMENT,
+  `question_id` int(32) NOT NULL,
+  `answer_content` text,
+  `is_right` tinyint(4) DEFAULT '0',
+  PRIMARY KEY (`answer_id`),
+  KEY `question_id_idx` (`question_id`),
+  CONSTRAINT `question_id` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `answers`
+--
+
+LOCK TABLES `answers` WRITE;
+/*!40000 ALTER TABLE `answers` DISABLE KEYS */;
+INSERT INTO `answers` VALUES (51,1,'Cache',1),(52,1,'Hard drive',1),(53,2,'Structured Query Language',0),(54,3,'Create Read Update Delete',0),(55,4,'one-to-one',1),(56,4,'multiple-to-zero',0),(57,4,'parent-to-child',0),(58,4,'one-to-many',1),(59,4,'container-to-data',0),(60,4,'many-to-many',1),(61,5,'127.0.0.1',0),(62,6,'JSON Database Comunication',0),(63,6,'Java Database Conectivity',1),(64,6,'JavaDatabase Comunication',0),(65,7,'Java Management Extention',0),(66,8,'@RestController',0),(67,8,'Presentation',0),(68,8,'@Sevice',0),(69,8,'Business logic',0),(70,8,'@Repository',0),(71,8,'Data access',0),(72,9,'TRUE',0),(73,10,'TRUE',0),(74,11,'Remote Procedure Call',0),(75,12,'Distributed event streaming platform',1),(76,12,'Message queue',0),(77,12,'Database',0);
+/*!40000 ALTER TABLE `answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `connectquestionsanswers`
+--
+
+DROP TABLE IF EXISTS `connectquestionsanswers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `connectquestionsanswers` (
+  `connect_question_answer_id` int(32) NOT NULL AUTO_INCREMENT,
+  `answer_id` int(32) NOT NULL,
+  `correct_pair_answer_id` int(32) NOT NULL,
+  PRIMARY KEY (`connect_question_answer_id`),
+  UNIQUE KEY `answer_id_UNIQUE` (`answer_id`),
+  UNIQUE KEY `correct_pair_answer_id_UNIQUE` (`correct_pair_answer_id`),
+  CONSTRAINT `answer_id0` FOREIGN KEY (`answer_id`) REFERENCES `answers` (`answer_id`),
+  CONSTRAINT `correct_pair_answer_id` FOREIGN KEY (`correct_pair_answer_id`) REFERENCES `answers` (`answer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `connectquestionsanswers`
+--
+
+LOCK TABLES `connectquestionsanswers` WRITE;
+/*!40000 ALTER TABLE `connectquestionsanswers` DISABLE KEYS */;
+INSERT INTO `connectquestionsanswers` VALUES (1,66,67),(2,68,69),(3,70,71);
+/*!40000 ALTER TABLE `connectquestionsanswers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `courses` (
+  `course_id` int(32) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` longtext NOT NULL,
+  PRIMARY KEY (`course_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `courses`
+--
+
+LOCK TABLES `courses` WRITE;
+/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
+INSERT INTO `courses` VALUES (5,'DB basics','Some essential skills, that will help you in future development'),(6,'Java advanced','Course that will be helpful for junior and middle devs, who want to improve their Java skills'),(7,'Java for web','You will learn basic Java frameworks fo web development (Spring, Dropwizard)'),(8,'Advanced Java for web','Proffesional tools for web. (Redis, Apache Kafka, gRPC, HTTP/2, WebSockets)');
+/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `groups` (
+  `group_id` int(32) NOT NULL AUTO_INCREMENT,
+  `moderator_id` int(32) DEFAULT NULL,
+  `group_name` varchar(45) NOT NULL,
+  PRIMARY KEY (`group_id`),
+  KEY `moderator_id_idx` (`moderator_id`),
+  CONSTRAINT `moderator_id` FOREIGN KEY (`moderator_id`) REFERENCES `moderators` (`moderator_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groups`
+--
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` VALUES (4,NULL,'developers'),(5,NULL,'lifetime -learners');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `moderators`
+--
+
+DROP TABLE IF EXISTS `moderators`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `moderators` (
+  `moderator_id` int(32) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  PRIMARY KEY (`moderator_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `moderators`
+--
+
+LOCK TABLES `moderators` WRITE;
+/*!40000 ALTER TABLE `moderators` DISABLE KEYS */;
+INSERT INTO `moderators` VALUES (2,'Vasiliy','Popovich');
+/*!40000 ALTER TABLE `moderators` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `questions` (
+  `question_id` int(32) NOT NULL AUTO_INCREMENT,
+  `test_id` int(32) NOT NULL,
+  `content` mediumtext NOT NULL,
+  `question_type` varchar(45) NOT NULL COMMENT 'only ''TRUE_FALSE'', ''TEST'', ''OPEN'', ''CONNECT'' are valid values',
+  PRIMARY KEY (`question_id`),
+  KEY `test_id_idx` (`test_id`),
+  CONSTRAINT `test_id` FOREIGN KEY (`test_id`) REFERENCES `tests` (`test_id`),
+  CONSTRAINT `question_type` CHECK ((`question_type` in (_utf8mb4'TRUE_FALSE',_utf8mb4'TEST',_utf8mb4'OPEN',_utf8mb4'CONNECT')))
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,1,'Where databases store data?','TEST'),(2,2,'What SQL stands for?','OPEN'),(3,2,'What CRUD stands for?','OPEN'),(4,3,'Check existing relations types','TEST'),(5,4,'Enter \"localhost\" IP address (e.g. 100.0.0.0)','OPEN'),(6,5,'What JDBC stands for?','TEST'),(7,5,'What JMX stands for?','OPEN'),(8,7,'Connect annotations to appropriate layers of application','CONNECT'),(9,8,'Should layered architecture always have 3 tiers?','TRUE_FALSE'),(10,9,'Do DropWizard always use Jetty as embedded server?','TRUE_FALSE'),(11,10,'What RPC stands for?','OPEN'),(12,11,'What is Kafka','TEST');
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `studentconnectquestionsanswers`
+--
+
+DROP TABLE IF EXISTS `studentconnectquestionsanswers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `studentconnectquestionsanswers` (
+  `student_connect_questions_answer_id` int(32) NOT NULL AUTO_INCREMENT,
+  `student_id` int(32) NOT NULL,
+  `answer_id` int(32) NOT NULL,
+  `pair_answer_id` int(32) NOT NULL,
+  `submited_time` timestamp(6) NOT NULL,
+  PRIMARY KEY (`student_connect_questions_answer_id`),
+  UNIQUE KEY `pair_answer_id_UNIQUE` (`pair_answer_id`),
+  UNIQUE KEY `answer_id_UNIQUE` (`answer_id`),
+  KEY `student_id_idx` (`student_id`),
+  CONSTRAINT `answer_id1` FOREIGN KEY (`answer_id`) REFERENCES `answers` (`answer_id`),
+  CONSTRAINT `pair_answer_id` FOREIGN KEY (`pair_answer_id`) REFERENCES `answers` (`answer_id`),
+  CONSTRAINT `student_id2` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studentconnectquestionsanswers`
+--
+
+LOCK TABLES `studentconnectquestionsanswers` WRITE;
+/*!40000 ALTER TABLE `studentconnectquestionsanswers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `studentconnectquestionsanswers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `studentopenquestionanswers`
+--
+
+DROP TABLE IF EXISTS `studentopenquestionanswers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `studentopenquestionanswers` (
+  `result_id` int(32) NOT NULL AUTO_INCREMENT,
+  `question_id` int(32) NOT NULL,
+  `student_id` int(32) NOT NULL,
+  `answer` mediumtext NOT NULL,
+  `submited_time` timestamp(6) NOT NULL,
+  PRIMARY KEY (`result_id`),
+  KEY `student_id_idx` (`student_id`),
+  KEY `question_id_idx` (`question_id`),
+  CONSTRAINT `question_id0` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`),
+  CONSTRAINT `student_id1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studentopenquestionanswers`
+--
+
+LOCK TABLES `studentopenquestionanswers` WRITE;
+/*!40000 ALTER TABLE `studentopenquestionanswers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `studentopenquestionanswers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `students` (
+  `student_id` int(32) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `group_id` int(32) DEFAULT NULL,
+  PRIMARY KEY (`student_id`),
+  KEY `group_id_idx` (`group_id`),
+  CONSTRAINT `group_id` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'Ihor','Polataiko',NULL),(2,'Ed','Miller',NULL),(3,'John','Brain',NULL),(4,'Martin','Fowler',NULL),(5,'Mary','Williams',NULL),(6,'Kate','Devis',NULL),(7,'Ann','Anderson',NULL),(8,'Antony','Joshua',NULL),(9,'Kate','Sierra',NULL),(10,'Alan','Brown',NULL),(11,'Alice','Liddell',NULL),(12,'Dale','Smith',NULL);
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `studentscourses`
+--
+
+DROP TABLE IF EXISTS `studentscourses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `studentscourses` (
+  `student_id` int(32) NOT NULL,
+  `course_id` int(32) NOT NULL,
+  `completed_date` datetime DEFAULT NULL,
+  KEY `student_id_idx` (`student_id`),
+  KEY `course_id_idx` (`course_id`),
+  CONSTRAINT `course_id0` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`),
+  CONSTRAINT `student_id0` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studentscourses`
+--
+
+LOCK TABLES `studentscourses` WRITE;
+/*!40000 ALTER TABLE `studentscourses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `studentscourses` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `studentstests`
+--
+
+DROP TABLE IF EXISTS `studentstests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `studentstests` (
+  `students_test_id` int(32) NOT NULL AUTO_INCREMENT,
+  `student_id` int(32) NOT NULL,
+  `test_id` int(32) NOT NULL,
+  `completed_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`students_test_id`),
+  KEY `student_id_idx` (`student_id`),
+  KEY `test_id_idx` (`test_id`),
+  CONSTRAINT `student_id3` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`),
+  CONSTRAINT `test_id1` FOREIGN KEY (`test_id`) REFERENCES `tests` (`test_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studentstests`
+--
+
+LOCK TABLES `studentstests` WRITE;
+/*!40000 ALTER TABLE `studentstests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `studentstests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `studenttestanswers`
+--
+
+DROP TABLE IF EXISTS `studenttestanswers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `studenttestanswers` (
+  `result_id` int(32) NOT NULL AUTO_INCREMENT,
+  `student_id` int(32) NOT NULL,
+  `answer_id` int(32) NOT NULL,
+  `submited_time` timestamp(6) NOT NULL,
+  PRIMARY KEY (`result_id`),
+  KEY `student_id_idx` (`student_id`),
+  KEY `answer_id_idx` (`answer_id`),
+  CONSTRAINT `answer_id` FOREIGN KEY (`answer_id`) REFERENCES `answers` (`answer_id`),
+  CONSTRAINT `student_id` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `studenttestanswers`
+--
+
+LOCK TABLES `studenttestanswers` WRITE;
+/*!40000 ALTER TABLE `studenttestanswers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `studenttestanswers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tests`
+--
+
+DROP TABLE IF EXISTS `tests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `tests` (
+  `test_id` int(32) NOT NULL AUTO_INCREMENT,
+  `course_id` int(32) NOT NULL,
+  `topic_title` varchar(255) NOT NULL,
+  PRIMARY KEY (`test_id`),
+  KEY `course_id_idx` (`course_id`),
+  CONSTRAINT `course_id` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tests`
+--
+
+LOCK TABLES `tests` WRITE;
+/*!40000 ALTER TABLE `tests` DISABLE KEYS */;
+INSERT INTO `tests` VALUES (1,5,'What is db?'),(2,5,'Query language'),(3,5,'Relations'),(4,6,'Connection to sockets'),(5,6,'Connection to DB'),(6,6,'JMX tools'),(7,7,'Spring MVC essentials'),(8,7,'Web architecture'),(9,7,'DropWizard'),(10,8,'gRPC, HTTP/2'),(11,8,'Apache Kafka');
+/*!40000 ALTER TABLE `tests` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-15 19:22:27
